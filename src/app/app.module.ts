88f4fe6215972e91aca848be2a400098f21982bf
AsaP <alberto83980654@gmail.com>
2022-01-25 00:13:18 -0600

    examen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,16 @@ import { AppRoutingModule } from './app.router';
 import { PublicModule } from './public/public.module';
 import { PrivateModule } from './private/private.module';
 import { AuthModule } from './auth/auth.module';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RouterOutletComponent } from './shared/router-outlet/router-outlet.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    NavbarComponent,
+    RouterOutletComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/private/private.router.ts
+++ src/app/private/private.router.ts
@@ -1,14 +1,15 @@
 import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { PrivateComponent } from './private.component';
-import {HomeComponent } from './home/home.component'
-
-
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { RouterOutletComponent } from './shared/router-outlet/router-outlet.component';
 
 const routes: Routes = [
-    { path: 'servidor', component: PrivateComponent,children:
-    [{path:'inicio', component: HomeComponent},
-     {path:'',redirectTo:'inicio', pathMatch:'full'}]},
+    { path: 'private', component: PrivateComponent,children:
+    [{path:'private', component: HomeComponent},
+     {path:'',redirectTo:'/private', pathMatch:'full'}]},
 ];
 
 @NgModule({
